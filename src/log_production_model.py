21924d81877c991ba0862f2f7d823c35232df35f
Urvi Gada <urvigada96@gmail.com>
2021-04-08 19:40:57 +0530

    Bug Fix

--- src/log_production_model.py
+++ src/log_production_model.py
@@ -6,12 +6,18 @@ from pprint import pprint
 import joblib
 import os
 
+
 def log_production_model(config_path):
     config = read_params(config_path)
+
     mlflow_config = config["mlflow_config"]
+
     model_name = mlflow_config["registered_model_name"]
+
     remote_server_uri = mlflow_config["remote_server_uri"]
+
     mlflow.set_tracking_uri(remote_server_uri)
+
     runs = mlflow.search_runs(experiment_ids=1)
     lowest = runs["metrics.mae"].sort_values(ascending=True)[0]
     lowest_run_id = runs[runs["metrics.mae"] == lowest]["run_id"][0]
@@ -24,18 +30,28 @@ def log_production_model(config_path):
             current_version = mv["version"]
             logged_model = mv["source"]
             pprint(mv, indent=4)
-            client.transition_model_version_stage(name=model_name, version=current_version, stage="Production")
+            client.transition_model_version_stage(
+                name=model_name,
+                version=current_version,
+                stage="Production"
+            )
         else:
             current_version = mv["version"]
-            client.transition_model_version_stage(name=model_name, version=current_version, stage="Staging")
+            client.transition_model_version_stage(
+                name=model_name,
+                version=current_version,
+                stage="Staging"
+            )
 
-    loaded_model = mlflow.pyfunc.log_model(logged_model)
-    model_path = config["webapp_model_dir"]
+    loaded_model = mlflow.pyfunc.load_model(logged_model)
+
+    model_path = config["webapp_model_dir"]  # "prediction_service/model"
 
     joblib.dump(loaded_model, model_path)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
-    log_production_model(config_path=parsed_args.config)
+    data = log_production_model(config_path=parsed_args.config)
